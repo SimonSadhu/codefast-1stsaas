e462f8e21f35b78589a397d0cd9a4d377dc67b92
SimonSadhu <you@example.com>
2025-02-03 11:06:34 -0300

    Updated hero center using tailwind css

--- app/page.js
+++ app/page.js
@@ -5,17 +5,19 @@ export default function Home() {
   const name = "Simon";
   return (
     <main>
-      <h1 className="text-3xl font-bold p-4">
-        Collect customer feedback to build better products
-      </h1>
-      <div>
-        Create a feedback board in minutes, prioritize features , and build
-        products your customers will love.
-      </div>
+      <section className="text-center py-32 px-8 max-w-3xl mx-auto ">
+        <h1 className="text-4xl font-extrabold mb-6 ">
+          Collect customer feedback to build better products
+        </h1>
+        <div className="opacity-90 mb-10">
+          Create a feedback board in minutes, prioritize features , and build
+          products your customers will love.
+        </div>
 
-      <ButtonLogin isLoggedIn={isLoggedIn} name={name}>
-        <div>This is a test</div>
-      </ButtonLogin>
+        <ButtonLogin isLoggedIn={isLoggedIn} name={name}>
+          <div>This is a test</div>
+        </ButtonLogin>
+      </section>
     </main>
   );
 }
