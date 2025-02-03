129b12a2f2af0ec7bc5bf1dc914bd1d6d3086db8
SimonSadhu <you@example.com>
2025-02-03 10:40:35 -0300

    add css

--- app/page.js
+++ app/page.js
@@ -1,13 +1,21 @@
-import Image from "next/image";
+import ButtonLogin from "@/components/ButtonLogin";
 
 export default function Home() {
+  const isLoggedIn = true;
+  const name = "Simon";
   return (
     <main>
-      <h1>Collect customer feedback to build better products</h1>
+      <h1 className="text-3xl font-bold p-4">
+        Collect customer feedback to build better products
+      </h1>
       <div>
         Create a feedback board in minutes, prioritize features , and build
         products your customers will love.
       </div>
+
+      <ButtonLogin isLoggedIn={isLoggedIn} name={name}>
+        <div>This is a test</div>
+      </ButtonLogin>
     </main>
   );
 }
---
+++ components/ButtonLogin.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+const ButtonLogin = ({ isLoggedIn, name }) => {
+  if (isLoggedIn) {
+    return <Link href="/dashboard">Welcome Back {name}</Link>;
+  }
+  return <button>Login</button>;
+};
+
+export default ButtonLogin;
